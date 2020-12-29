3a544c98cb5a69cb77935b25aabc5900c730255f
vladkirov <vladkirov@gmail.com>
2020-12-30 00:04:51 +0300

    Final2

--- src/main/java/com/example/examination04/specifications/ClimberSpecification.java
+++ src/main/java/com/example/examination04/specifications/ClimberSpecification.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.domain.Specification;
 public class ClimberSpecification {
     public static Specification<Climber> climbersByAge(int from, int by) {
         return (root, query, criteriaBuilder) ->
-                criteriaBuilder.between(root.get(Climber_.AGE), from, by);
+                criteriaBuilder.between(root.get(Climber_.age), from, by - 1);
     }
 }
